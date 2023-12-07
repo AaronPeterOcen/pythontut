60542db7d23e35cdffa91a7cff54554f375f10ac
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-12-07 20:57:57 +0300

    update on gen funcs

--- generators/generators.py
+++ generators/generators.py
@@ -1,5 +1,19 @@
-# Generators
+# Generators  is a function that returns an iterator
+# using the Yield keyword. In this article
 # Range is one of them
+
+
+# A generator function that yields 1 for first time,
+# 2 second time and 3 third time
+def simpleGeneratorFun():
+    yield 1
+    yield 2
+    yield 3
+
+
+# Driver code to check above generator function
+for value in simpleGeneratorFun():
+    print(value)
 
 
 # syntax of the generator
